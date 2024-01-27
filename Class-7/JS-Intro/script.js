27cad735f43d7b71f032e93b9e572cb332663243
Sabeel Khan <sabeelhps@gmail.com>
2024-01-27 21:52:03 +0530

    Bootstrap and Intro to JS

---
+++ Class-7/JS-Intro/script.js
@@ -0,0 +1,25 @@
+console.log('Welcome to GFG!');
+
+
+// Primitives - Datatypes which cannot be broken down into simpler datatypes.
+
+// 1. Number - (100, 0, -999, 3.14, -89.54, Infinity,  -Infinity, NaN)
+// 2. String
+// 3. Boolean - true/false
+// 4. null
+// 5. undefined
+
+
+// What is console ? --> REPL => Read, Evaluate, Print, Loop
+
+
+
+// We can perform all the Arithmatic Operations on Numbers
+
+// 1. Addition (+)
+// 2. Sub (-)
+// 3. Division (/)
+// 4. Multiplication (*)
+
+
+// H.W : Figure out the priority associated with different operaters
